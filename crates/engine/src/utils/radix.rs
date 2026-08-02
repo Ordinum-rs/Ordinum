14ad4909c4a391e67b6530eba5c0ae52ecda188a
Kristian <kristian1599@gmail.com>
2026-08-02 20:31:54 +0100

    more work on radix tree

--- crates/engine/src/utils/radix.rs
+++ crates/engine/src/utils/radix.rs
@@ -17,66 +17,256 @@
  * 4. x has no common prefix --> Edge: NODE | X: GEODE
  *
  *
+ * Memory:
+ *
+ *
+ *
+ *
+ *
  *
  *
  */
+use mem::hazard::Pointer;
+use std::{array, marker::PhantomData, mem::MaybeUninit, ptr::NonNull};
 
-use std::{borrow::Cow, ptr::NonNull};
+// --- SLAB --- //
 
-struct RadixTree {
-    sentinel: NonNull<Node>,
+struct Slot<T> {
+    value: MaybeUninit<T>,
+    occupied: bool,
+    next_free: Option<NonNull<Slot<T>>>,
 }
 
-impl RadixTree {
-    // TODO: Check
-    fn new() -> Self {
-        let node = unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(Node::new()))) };
-        Self { sentinel: node }
-    }
-
-    fn search<X: AsRef<[u8]>>(&self, x: X) -> Option<Cow<'_, [u8]>> {
-        Some(Cow::from(b"hello"))
-    }
-}
-
-struct Edge {
-    next: (), // Ptr to Boxed Node?
-    label: (),
-}
-
-struct Node {
-    edges: (),
-    is_lead: (),
-}
-
-impl Node {
+impl<T> Slot<T> {
     fn new() -> Self {
         Self {
-            edges: (),
-            is_lead: (),
+            value: MaybeUninit::uninit(),
+            occupied: false,
+            next_free: None,
+        }
+    }
+}
+
+struct ChunkSlab<T, const CHUNK_SIZE: usize> {
+    chunks: Vec<Box<[Slot<T>; CHUNK_SIZE]>>,
+    free: Vec<NonNull<Slot<T>>>,
+}
+
+impl<T, const CHUNK_SIZE: usize> ChunkSlab<T, CHUNK_SIZE> {
+    //
+    fn new() -> Self {
+        // Will start off
+        // []
+        // []
+        // Lazily allocate a chunk on first contact
+        Self {
+            chunks: Vec::new(),
+            free: Vec::new(),
+        }
+    }
+    //
+    fn allocate_chunk(&mut self) {
+        let chunk: Box<[Slot<T>; CHUNK_SIZE]> = Box::new(array::from_fn(|_| Slot::new()));
+        chunk.map(|i| self.free.push(NonNull::from(&i)));
+    }
+}
+
+// -----------------
+
+#[derive(Clone, Copy)]
+struct NodePtr {
+    ptr: NonNull<()>,
+}
+
+#[repr(transparent)]
+struct LeafPtr(NodePtr);
+
+#[repr(C)]
+struct NodeHeader {
+    label: Box<[u8]>,
+    children: u16,
+    terminal: Option<LeafPtr>,
+}
+
+#[repr(C, align(8))]
+struct LinearChild<const INDEXES: usize> {
+    edges: [u8; INDEXES],
+    children: [Option<NodePtr>; INDEXES],
+}
+
+impl<const INDEXES: usize> LinearChild<INDEXES> {
+    fn new() -> Self {
+        Self {
+            edges: [0u8; INDEXES],
+            children: [None; INDEXES],
+        }
+    }
+}
+
+#[repr(C, align(8))]
+struct IndexedChild<const INDEXES: usize, const CHILDREN: usize> {
+    edges: [u8; INDEXES],
+    children: [Option<NodePtr>; CHILDREN],
+}
+
+impl<const INDEXES: usize, const CHILDREN: usize> IndexedChild<INDEXES, CHILDREN> {
+    fn new() -> Self {
+        Self {
+            edges: [0u8; INDEXES],
+            children: [None; CHILDREN],
+        }
+    }
+}
+
+#[repr(C, align(8))]
+struct DirectChild<const CHILD_LEN: usize> {
+    children: [Option<NodePtr>; CHILD_LEN],
+}
+
+impl<const CHILD_LEN: usize> DirectChild<CHILD_LEN> {
+    fn new() -> Self {
+        Self {
+            children: [None; CHILD_LEN],
+        }
+    }
+}
+
+#[repr(C, align(8))]
+struct Leaf<T> {
+    key: Box<[u8]>,
+    value: T,
+}
+
+#[repr(C)]
+struct Node<C> {
+    header: NodeHeader,
+    children: C,
+}
+
+impl<C> Node<C> {
+    fn new(children: C) -> Self {
+        Self {
+            header: NodeHeader {
+                label: Box::new([0u8; 0]),
+                children: 0,
+                terminal: None,
+            },
+            children,
+        }
+    }
+}
+
+type Node4 = Node<LinearChild<4>>;
+impl Default for Node4 {
+    fn default() -> Self {
+        Node::new(LinearChild::new())
+    }
+}
+type Node16 = Node<LinearChild<16>>;
+impl Default for Node16 {
+    fn default() -> Self {
+        Node::new(LinearChild::new())
+    }
+}
+type Node48 = Node<IndexedChild<48, 256>>;
+impl Default for Node48 {
+    fn default() -> Self {
+        Node::new(IndexedChild::new())
+    }
+}
+type Node256 = Node<DirectChild<256>>;
+impl Default for Node256 {
+    fn default() -> Self {
+        Node::new(DirectChild::new())
+    }
+}
+
+// Node Kinds
+
+#[repr(u8)]
+enum NodeKind {
+    Node4,
+    Node16,
+    Node48,
+    Node256,
+    Leaf,
+}
+
+// Each entry to be wrapped by the slab
+struct NodeStore<T> {
+    node4: ChunkSlab<Node4, 12>,
+    node16: ChunkSlab<Node16, 12>,
+    node48: ChunkSlab<Node48, 8>,
+    node256: ChunkSlab<Node256, 2>,
+    leaf: ChunkSlab<Leaf<T>, 16>,
+}
+
+impl<T> NodeStore<T> {
+    fn new() -> Self {
+        Self {
+            node4: ChunkSlab::new(),
+            node16: ChunkSlab::new(),
+            node48: ChunkSlab::new(),
+            node256: ChunkSlab::new(),
+            leaf: ChunkSlab::new(),
         }
     }
 }
 
 #[test]
-fn borrow() {
-    // TODO: Move to &'_ [u8] instead
-    let tree = RadixTree::new();
+fn size_estimates() {
+    // TODO: Keep workiing size estimates
 
-    let mut word = tree.search(b"world").unwrap();
+    println!("{}", size_of::<Slot<Node4>>());
 
-    match &word {
-        Cow::Borrowed(_) => println!("Yay"),
-        Cow::Owned(_) => println!("Nay"),
+    let target = 16 * 1024;
+    let chunk_est = target / size_of::<Slot<Node4>>();
+    println!("{}", chunk_est);
+}
+
+#[test]
+fn ptr_casting() {
+    #[repr(C)]
+    struct Header {
+        flags: u8,
     }
 
-    word.to_mut().extend_from_slice(b"world");
-
-    println!("{:?}", word);
-    println!("{:?}", tree.search(b"world").unwrap());
-
-    match &word {
-        Cow::Borrowed(_) => println!("Yay"),
-        Cow::Owned(_) => println!("Nay"),
+    #[repr(C)]
+    struct Node1 {
+        header: Header,
+        num: u32,
+        // Padding 4 because 4 + 1 = 5 next alignment = 8
     }
+
+    #[repr(C)]
+    struct Node2 {
+        header: Header,
+        num: u64,
+        // Padding 7 because 1 + 8 = 9 next alignment = 16
+    }
+
+    assert_eq!(size_of::<Header>(), 1);
+    assert_eq!(size_of::<Node1>(), 8);
+    assert_eq!(size_of::<Node2>(), 16);
+
+    // Fake slab
+
+    struct Slot {
+        item: u8,
+    }
+
+    let mut slab: Vec<Box<[Slot]>> = Vec::new();
+}
+
+#[test]
+fn array_map() {
+    let slice: [usize; 5] = array::from_fn(|i| i + 1);
+
+    let mut vec: Vec<usize> = Vec::new();
+
+    dbg!(&vec);
+
+    slice.map(|x| vec.push(x));
+
+    dbg!(vec);
 }
